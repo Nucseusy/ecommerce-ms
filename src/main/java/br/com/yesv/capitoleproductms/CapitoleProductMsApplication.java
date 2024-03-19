32c46ec84e52a3fc6fcc3bbc443384829784d21e
Nucseusy <nucseusy@gmail.com>
2024-03-19 09:13:32 -0300

    Initial setup of the Spring Boot project

---
+++ src/main/java/br/com/yesv/capitoleproductms/CapitoleProductMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.yesv.capitoleproductms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitoleProductMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitoleProductMsApplication.class, args);
+    }
+
+}
